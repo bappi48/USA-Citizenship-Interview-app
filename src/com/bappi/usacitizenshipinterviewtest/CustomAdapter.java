77ae09f0c6d7a9065100280a3ab98d541a14c113
Tanvir Hussain <bappi.ruet.eee@gmail.com>
2015-08-02 00:49:25 +0600

    First working

---
+++ src/com/bappi/usacitizenshipinterviewtest/CustomAdapter.java
@@ -0,0 +1,81 @@
+package com.bappi.usacitizenshipinterviewtest;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class CustomAdapter extends BaseAdapter {
+
+	Context mainContext;
+	LayoutInflater inflater = null;
+	String strNum;
+	String strQuestion;
+	String strAnswer;
+	String packageName;
+    Resources resources;
+
+	
+	CustomAdapter(Context ctx){
+		mainContext = ctx;
+		packageName = mainContext.getPackageName();
+		resources = mainContext.getResources();
+		inflater = (LayoutInflater) mainContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		
+	}
+	
+	
+	
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 100;
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return arg0;
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return arg0;
+	}
+	
+	public class Holder{
+		TextView txtNum;
+		TextView txtQuestion;
+		TextView txtAnswer;
+	}
+
+	//int resId = getResources().getIdentifier(aString, "string", packageName);
+	
+	@Override
+	public View getView(int position, View view, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		Holder holder = new Holder();
+		View rowView = inflater.inflate(R.layout.listitem, null); 
+		holder.txtNum = (TextView) rowView.findViewById(R.id.number);
+		holder.txtQuestion = (TextView) rowView.findViewById(R.id.question);
+		holder.txtAnswer = (TextView) rowView.findViewById(R.id.answer);
+		
+		position++;
+		
+		strNum = "number" + position;
+		strQuestion = "question"+position;
+		strAnswer = "answer"+position;
+		
+		holder.txtNum.setText(resources.getIdentifier(strNum, "string", packageName));
+		holder.txtQuestion.setText(resources.getIdentifier(strQuestion, "string", packageName));
+		holder.txtAnswer.setText(resources.getIdentifier(strAnswer, "string", packageName));
+		
+		
+		return rowView;
+	}
+
+}
--- src/com/bappi/usacitizenshipinterviewtest/MainActivity.java
+++ src/com/bappi/usacitizenshipinterviewtest/MainActivity.java
@@ -1,16 +1,54 @@
 package com.bappi.usacitizenshipinterviewtest;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
 
 public class MainActivity extends Activity {
 
+    final MediaPlayer mp = new MediaPlayer();
+    AssetFileDescriptor afd;
+    String trackName;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.citizenship_question);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.main_view);		
+		
+		ListView listView = (ListView) findViewById(R.id.list);
+		listView.setAdapter(new CustomAdapter(getApplicationContext()));
+		
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int position,	long arg3) {
+				// TODO Auto-generated method stub
+				if(mp.isPlaying()){  
+					mp.stop();
+				} 
+
+				try {
+					mp.reset();		
+					position++;
+					trackName = "track"+ position + ".mp3";
+					afd = getAssets().openFd(trackName);
+					mp.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+					mp.prepare();
+					mp.start();
+				} catch (Exception e) {
+					
+				} 			
+				
+			}			
+			
+		});
+		
 	}
-
-
 
 }
