d7d41a657bdf03146a4ef665c8d0675a507bb5e3
Tanvir Hussain <bappi.ruet.eee@gmail.com>
2015-08-02 05:55:30 +0600

    senator, governors name added

--- src/com/bappi/usacitizenshipinterviewtest/MainActivity.java
+++ src/com/bappi/usacitizenshipinterviewtest/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends Activity {
 		listView.setAdapter(new CustomAdapter(getApplicationContext()));
 		
 		listView.setOnItemClickListener(new OnItemClickListener() {
-
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int position,	long arg3) {
 				// TODO Auto-generated method stub
